4a46537d06ce33717753283659ea4a20fee799f2
Benjamin Massey <benjamin.w.massey@gmail.com>
2022-05-14 12:31:51 -0700

    Added ramping up functionality to Rotater

--- Assets/Scripts/Rotater.cs
+++ Assets/Scripts/Rotater.cs
@@ -5,19 +5,47 @@ using UnityEngine;
 public class Rotater : MonoBehaviour
 {
     [SerializeField]
-    private float mSpeed = 50.0f;
+    private bool mRampUp = true;
+    [SerializeField]
+    private float mSpeed = 50f;
+    [SerializeField]
+    private float mSwapFrequency = 10f;
 
-    private Vector3 mDirection;
+    private float mStartTime;
+
+    private float mRampVal;
+
+    private float mDirection;
+
+    private int mSwapCount;
 
     // Start is called before the first frame update
     void Start()
     {
-        mDirection = Vector3.forward;
+        mStartTime = Time.time;
+        mRampVal = 0f;
+        mDirection = 1f;
+        mSwapCount = 0;
+        StartCoroutine("DirectionSwapper");
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(mDirection * Time.deltaTime * mSpeed);
+        mRampVal = ((mRampUp ? 1f : 0f) + (mSwapCount / 4f)) * Mathf.Sqrt(Time.time - mStartTime);
+        transform.Rotate(Vector3.forward * mDirection * Time.deltaTime * mSpeed * mRampVal);
+    }
+
+    IEnumerator DirectionSwapper()
+    {
+        while (true) {
+            yield return new WaitForSeconds(mSwapFrequency);
+            float prevDirection = mDirection;
+            mDirection = 0f;
+            yield return new WaitForSeconds(0.5f);
+            mStartTime = Time.time;
+            mDirection = prevDirection * -1f;
+            mSwapCount++;
+        }
     }
 }
