e2b8aa0209047cb61eb3b4d0ab22edd2cd2878c0
Benjamin Massey <benjamin.w.massey@gmail.com>
2022-05-27 10:08:32 -0700

    Have character image loading and swapping between them in Selection. Strong note that this currently does nothing, AND needs to be handled on a per player basis, but hey it's a start.

--- Assets/Scripts/Selection.cs
+++ Assets/Scripts/Selection.cs
@@ -8,11 +8,20 @@ public class Selection : MonoBehaviour
 {
     [SerializeField]
     private GameObject mTextObj;
+    [SerializeField]
+    private GameObject mImageObj;
+
+    private Vector2 mImgSize = new Vector2(400f, 400f);
+
+    private Sprite[] mCharacterSprites;
+
+    private Vector3 mInitialScale;
 
     private enum Setting
     { 
       Rounds,
-      Players
+      Players,
+      Characters
     };
 
     private Setting mCurrent;
@@ -24,12 +33,15 @@ public class Selection : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        mCharacterSprites = Resources.LoadAll<Sprite>("Characters");
+        mInitialScale = mImageObj.transform.localScale;
         mTextObj.SetActive(true);
         mCurrent = Setting.Rounds;
         mHeader = new Dictionary<Setting, string>()
         {
             { Setting.Rounds, "Select the number of rounds." },
-            { Setting.Players, "Select the number of players." }
+            { Setting.Players, "Select the number of players." },
+            { Setting.Characters, "Select your character." }
         };
     }
 
@@ -49,21 +61,54 @@ public class Selection : MonoBehaviour
             }
             else if (mCurrent == Setting.Players)
             {
-                // TODO: actual stuff
+                // TODO: apply players info
+
+                mImageObj.SetActive(true);
+                mNum = -1; // silly workaround for initial update
+                mCurrent = Setting.Characters;
+            }
+            else if (mCurrent == Setting.Characters)
+            {
+                // TODO: apply character and handle other players and etc
                 End();
             }
         }
         else 
         {
-            mNum += Input.GetKeyDown(JoyCon.SR()) ? 1 : 0;
-            mNum -= Input.GetKeyDown(JoyCon.SL()) ? 1 : 0;
-            mNum = Mathf.Max(mNum, 0);
-            string msg = mHeader[mCurrent] + "\n\n";
-            if (mNum > 0) msg += "< ";
-            else msg += "  ";
-            msg += mNum.ToString();
-            msg += " >";
-            mTextObj.GetComponent<Text>().text = msg;
+            if (mCurrent == Setting.Rounds || mCurrent == Setting.Players)
+            {
+                mNum += Input.GetKeyDown(JoyCon.SR()) ? 1 : 0;
+                mNum -= Input.GetKeyDown(JoyCon.SL()) ? 1 : 0;
+                mNum = Mathf.Max(mNum, 0);
+                string msg = mHeader[mCurrent] + "\n\n";
+                if (mNum > 0) msg += "< ";
+                else msg += "  ";
+                msg += mNum.ToString();
+                msg += " >";
+                mTextObj.GetComponent<Text>().text = msg;
+            }
+            else if (mCurrent == Setting.Characters)
+            {
+                int prevNum = mNum;
+                mNum += Input.GetKeyDown(JoyCon.SR()) ? 1 : 0;
+                mNum -= Input.GetKeyDown(JoyCon.SL()) ? 1 : 0;
+                if (mNum <= -1) mNum = mCharacterSprites.Length - 1;
+                if (mNum >= mCharacterSprites.Length) mNum = 0;
+                if (mNum != prevNum || mNum == -1)
+                {
+                    mImageObj.GetComponent<SpriteRenderer>().sprite = mCharacterSprites[mNum];
+                    float scale_width = mImgSize.x / mCharacterSprites[mNum].texture.width;
+                    float scale_height = mImgSize.y / mCharacterSprites[mNum].texture.height;
+                    mImageObj.transform.localScale = new Vector3(mInitialScale.x * scale_width,
+                                                                 mInitialScale.y * scale_height,
+                                                                 mInitialScale.z);
+                    string name = mCharacterSprites[mNum].texture.name;
+                    name = name.Replace('_', ' ');
+                    string msg = mHeader[mCurrent] + "\n" + name + "\n\n\n\n\n\n\n";
+                    mTextObj.GetComponent<Text>().text = msg;
+                }
+                    
+            }
         }
     }
 
