8e0b12233e9aabf725024afc418cfcc202c10169
Benjamin Massey <benjamin.w.massey@gmail.com>
2022-05-27 10:43:15 -0700

    Have actual player joining, and player basis character selection. A note that this has not been tested with multiple JoyCons yet, and that the character selection is stored, but not yet used in game.

--- Assets/Scripts/GlobalState.cs
+++ Assets/Scripts/GlobalState.cs
@@ -4,7 +4,15 @@ using UnityEngine;
 
 public static class GlobalState
 {
+    // how many rounds to do, to be set by Selection (3 is legacy, but default good
     public static int end = 3;
 
+    // player values are joycon indices
+    public static int[] players;
+
+    // player value (GlobalState.players) => character sprite (image)
+    public static Dictionary<int, Sprite> characters = new Dictionary<int, Sprite>();
+
+    // player name (string) => score (int)
     public static Dictionary<string, int> scores = new Dictionary<string, int>();
 }
--- Assets/Scripts/Selection.cs
+++ Assets/Scripts/Selection.cs
@@ -28,7 +28,11 @@ public class Selection : MonoBehaviour
 
     private Dictionary<Setting, string> mHeader;
 
+    private List<int> mPlayerValues;
+
     private int mNum;
+
+    private int mCurrentPlayerIndex;
 
     // Start is called before the first frame update
     void Start()
@@ -40,16 +44,18 @@ public class Selection : MonoBehaviour
         mHeader = new Dictionary<Setting, string>()
         {
             { Setting.Rounds, "Select the number of rounds." },
-            { Setting.Players, "Select the number of players." },
+            { Setting.Players, "Press a face button to join the game." },
             { Setting.Characters, "Select your character." }
         };
+        mPlayerValues = new List<int>();
+        mNum = 0;
+        mCurrentPlayerIndex = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(JoyCon.X()) || 
-            Input.GetKeyDown(JoyCon.Plus()) ||
+        if (Input.GetKeyDown(JoyCon.Plus()) ||
             Input.GetKeyDown(JoyCon.Minus()))
         {
             if (mCurrent == Setting.Rounds)
@@ -61,21 +67,20 @@ public class Selection : MonoBehaviour
             }
             else if (mCurrent == Setting.Players)
             {
-                // TODO: apply players info
-
+                GlobalState.players = mPlayerValues.ToArray();
                 mImageObj.SetActive(true);
                 mNum = -1; // silly workaround for initial update
                 mCurrent = Setting.Characters;
             }
             else if (mCurrent == Setting.Characters)
             {
-                // TODO: apply character and handle other players and etc
-                End();
+                if (GlobalState.players.Length == GlobalState.characters.Count)
+                    End();
             }
         }
         else 
         {
-            if (mCurrent == Setting.Rounds || mCurrent == Setting.Players)
+            if (mCurrent == Setting.Rounds)
             {
                 mNum += Input.GetKeyDown(JoyCon.SR()) ? 1 : 0;
                 mNum -= Input.GetKeyDown(JoyCon.SL()) ? 1 : 0;
@@ -87,27 +92,61 @@ public class Selection : MonoBehaviour
                 msg += " >";
                 mTextObj.GetComponent<Text>().text = msg;
             }
+            else if (mCurrent == Setting.Players)
+            {
+                for (int i = 1; i <= 8; i++)
+                {
+                    if (Input.GetKeyDown(JoyCon.A(i)) || Input.GetKeyDown(JoyCon.B(i)) ||
+                        Input.GetKeyDown(JoyCon.X(i)) || Input.GetKeyDown(JoyCon.Y(i)))
+                    {
+                        if (!mPlayerValues.Contains(i))
+                        {
+                            mPlayerValues.Add(i);
+                            mNum++;
+                        }
+                    }
+                }
+                string msg = mHeader[mCurrent] + "\n";
+                msg += "Number of Players: " + mNum.ToString();
+                mTextObj.GetComponent<Text>().text = msg;
+            }
             else if (mCurrent == Setting.Characters)
             {
-                int prevNum = mNum;
-                mNum += Input.GetKeyDown(JoyCon.SR()) ? 1 : 0;
-                mNum -= Input.GetKeyDown(JoyCon.SL()) ? 1 : 0;
-                if (mNum <= -1) mNum = mCharacterSprites.Length - 1;
-                if (mNum >= mCharacterSprites.Length) mNum = 0;
-                if (mNum != prevNum || mNum == -1)
+                if (mCurrentPlayerIndex < GlobalState.players.Length &&
+                    Input.GetKeyDown(JoyCon.X(GlobalState.players[mCurrentPlayerIndex])))
                 {
-                    mImageObj.GetComponent<SpriteRenderer>().sprite = mCharacterSprites[mNum];
-                    float scale_width = mImgSize.x / mCharacterSprites[mNum].texture.width;
-                    float scale_height = mImgSize.y / mCharacterSprites[mNum].texture.height;
-                    mImageObj.transform.localScale = new Vector3(mInitialScale.x * scale_width,
-                                                                 mInitialScale.y * scale_height,
-                                                                 mInitialScale.z);
-                    string name = mCharacterSprites[mNum].texture.name;
-                    name = name.Replace('_', ' ');
-                    string msg = mHeader[mCurrent] + "\n" + name + "\n\n\n\n\n\n\n";
-                    mTextObj.GetComponent<Text>().text = msg;
+                    GlobalState.characters.Add(GlobalState.players[mCurrentPlayerIndex], mCharacterSprites[mNum]);
+                    mCurrentPlayerIndex++;
                 }
-                    
+                else
+                {
+                    if (mCurrentPlayerIndex < GlobalState.players.Length)
+                    {
+                        int prevNum = mNum;
+                        mNum += Input.GetKeyDown(JoyCon.SR(GlobalState.players[mCurrentPlayerIndex])) ? 1 : 0;
+                        mNum -= Input.GetKeyDown(JoyCon.SL(GlobalState.players[mCurrentPlayerIndex])) ? 1 : 0;
+                        if (mNum <= -1) mNum = mCharacterSprites.Length - 1;
+                        if (mNum >= mCharacterSprites.Length) mNum = 0;
+                        if (mNum != prevNum || mNum == -1)
+                        {
+                            mImageObj.GetComponent<SpriteRenderer>().sprite = mCharacterSprites[mNum];
+                            float scale_width = mImgSize.x / mCharacterSprites[mNum].texture.width;
+                            float scale_height = mImgSize.y / mCharacterSprites[mNum].texture.height;
+                            mImageObj.transform.localScale = new Vector3(mInitialScale.x * scale_width,
+                                                                         mInitialScale.y * scale_height,
+                                                                         mInitialScale.z);
+                            string name = mCharacterSprites[mNum].texture.name;
+                            name = name.Replace('_', ' ');
+                            string msg = mHeader[mCurrent] + "\n" + name + "\n\n\n\n\n\n\n";
+                            mTextObj.GetComponent<Text>().text = msg;
+                        }
+                    }
+                    else
+                    {
+                        mImageObj.SetActive(false);
+                        mTextObj.GetComponent<Text>().text = "All players have selected their characters.";
+                    }
+                }
             }
         }
     }
